0bfa384a614a09b7ed9a7834184995afef89f045
Adam Alton <adamalton@gmail.com>
2014-08-11 21:01:00 +0100

    Remove unused import.

--- registration/tests/test_simple_backend.py
+++ registration/tests/test_simple_backend.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.auth.tests.utils import skipIfCustomUser
 from django.core.urlresolvers import reverse
